4f2d205dd554ce382ceae840ebc6d2223c797c2b
Sean Yu <seansu4you87@gmail.com>
2013-08-16 16:41:28 -0700

    First commit!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GrowthEmails::Application.config.secret_token = '365f351458e9e6419322370fb49990a294a6b26d6fffcf5ec91fc59bfcba92a8d18d31d46e36d85c6565128cdde7c3a54bf8cb3cea1b66f96264798a11520164'
